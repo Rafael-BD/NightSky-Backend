35d17efd425be3055e9dbc4bf09134886c781c16
Rafael-BD <rafael8885326@gmail.com>
2024-09-13 15:33:50 -0300

    feat: improve rate limiter middleware with enhanced request handling and error messages

--- app-services/src/middlewares/rateLimiterMiddleware.ts
+++ app-services/src/middlewares/rateLimiterMiddleware.ts
@@ -2,24 +2,23 @@ import { Context } from "https://deno.land/x/oak@v13.1.0/mod.ts";
 
 const kv = await Deno.openKv();
 
-const MAX_REQUESTS = 15;         
-const BLOCK_DURATION = 30;       
-const WINDOW_DURATION = 30;      
+const MAX_REQUESTS = 15;      
+const BLOCK_DURATION = 30;    
+const WINDOW_DURATION = 30;   
 
 export default async function rateLimiter(ctx: Context, next: () => Promise<unknown>) {
     const ip = ctx.request.ip || ctx.request.headers.get('x-forwarded-for') || 'unknown';
-    const currentTime = Math.floor(Date.now() / 1000);
+    const currentTime = Math.floor(Date.now() / 1000); // seconds
 
     const key = `rate_limit:${ip}`; 
     const blockedKey = `blocked:${ip}`; 
 
     const isBlocked = await kv.get([blockedKey]);
-    console.log(isBlocked);
 
-    if (isBlocked) {
+    if (isBlocked?.value && parseInt(String(isBlocked.value), 10) > currentTime) {
         ctx.response.status = 429;
         ctx.response.body = {
-            error: `Too many requests.`,
+            error: `Too many requests. Please try again later.`,
         };
         return;
     }
@@ -28,19 +27,17 @@ export default async function rateLimiter(ctx: Context, next: () => Promise<unkn
     const newRequestCount = requestCount ? parseInt(String(requestCount.value), 10) + 1 : 1;
 
     if (newRequestCount === 1) {
-        await kv.set([key], newRequestCount.toString(), { expireIn: WINDOW_DURATION }); 
-    } else {
-        await kv.set([key], newRequestCount.toString(), { expireIn: WINDOW_DURATION }); 
-    }
-
-    console.log(newRequestCount);
-    if (newRequestCount > MAX_REQUESTS) {
-        await kv.set([blockedKey], (currentTime + BLOCK_DURATION).toString(), { expireIn: BLOCK_DURATION }); 
+        await kv.set([key], newRequestCount.toString(), { expireIn: WINDOW_DURATION });
+    } else if (newRequestCount > MAX_REQUESTS) {
+        await kv.set([blockedKey], (currentTime + BLOCK_DURATION).toString(), { expireIn: BLOCK_DURATION });
+        await kv.delete([key]); 
         ctx.response.status = 429;
         ctx.response.body = {
             error: `Too many requests.`,
         };
         return;
+    } else {
+        await kv.set([key], newRequestCount.toString(), { expireIn: WINDOW_DURATION });
     }
 
     await next();
