bc6c6b28d6e504957f25f1f8a216eb04e3a9a4b0
Rafael-BD <rafael8885326@gmail.com>
2024-09-21 17:56:11 -0300

    feat

--- plugin-processing/src/cron/config.ts
+++ plugin-processing/src/cron/config.ts
@@ -3,7 +3,7 @@ import analyzer from "../processing/pluginAnalysis.ts";
 
 export default function cron() {
 
-    Deno.cron("Plugin analysis", { minute: { every: 300 } }, () => {
+    Deno.cron("Plugin analysis", { hour: { every: 5 } }, () => {
         analyzer();
     });
 
