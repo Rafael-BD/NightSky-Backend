ab1186dcfd106e69b2ada6051c5cbeb5adfd4fad
Rafael-BD <rafael8885326@gmail.com>
2024-09-22 22:50:40 -0300

    feat: add logging for extracted files in plugin analysis

--- plugin-processing/src/processing/pluginAnalysis.ts
+++ plugin-processing/src/processing/pluginAnalysis.ts
@@ -64,6 +64,7 @@ export default async function analyzer() {
 
         // Update appmanifest.json with new version code and plugin ID
         const rootFolder = Object.keys(extractedFiles)[0];
+        console.log("Extracted files:", extractedFiles);
         console.log("Root folder:", extractedFiles[rootFolder] as FileStructure);
         const appManifest = (extractedFiles[rootFolder] as FileStructure)["appmanifest.json"] as Uint8Array;
         let filesUpdated = extractedFiles;
