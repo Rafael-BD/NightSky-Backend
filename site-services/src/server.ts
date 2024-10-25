aed78f403ddfa104622849ed9616a9bcfa95992a
Rafael-BD <rafael8885326@gmail.com>
2024-10-25 00:23:12 -0300

    feat: enable rate limiting and add CORS support to the application

--- site-services/src/server.ts
+++ site-services/src/server.ts
@@ -1,4 +1,5 @@
 import { Application } from "https://deno.land/x/oak@v13.1.0/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import pluginRoutes from "./routes/pluginsRoutes.ts";
 import authRoutes from "./routes/authRoutes.ts";
 import validateMiddleware from "./middlewares/validateMiddleware.ts";
@@ -6,13 +7,20 @@ import rateLimiter from "../../shared/utils/rateLimiterMiddleware.ts";
 
 const app = new Application();
 
+app.use(
+    oakCors({
+        origin: "*",
+        optionsSuccessStatus: 200,
+    }),
+);
+
 app.use(validateMiddleware);
-// app.use(rateLimiter);
+app.use(rateLimiter);
 
 app.use(authRoutes.routes());
 app.use(authRoutes.allowedMethods());
 app.use(pluginRoutes.routes());
 app.use(pluginRoutes.allowedMethods());
 
-console.log("App Operations Backend running on http://localhost:8001");
+console.log("App Operations Backend running");
 await app.listen({ port: 8001 });;
